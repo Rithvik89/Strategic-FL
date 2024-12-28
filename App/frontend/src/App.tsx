335e52a708379e0448b01d5ec2cd4afbe51a8fd9
Rithvik89 <rithvikalkanti@gmail.com>
2024-12-28 17:55:48 +0530

    Add routing and screens for user profile, leagues, and league creation in App component

--- App/frontend/src/App.tsx
+++ App/frontend/src/App.tsx
@@ -5,42 +5,102 @@ import { CardLayout } from './components/PlayerCard';
 import { TradeScreen } from './screens/TradeScreen';
 import RightFooter from './components/RightFooter.tsx';
 import { LeftFooter } from './components/LeftFooter.tsx';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { LeaguesScreen } from './screens/LeaguesScreen';
+import { CreateLeague } from './screens/CreateLeague';
+import UserProfile from './screens/UserProfile';
+import Header from './components/Header';
+import MobileHeader from './components/MobileHeader';
+import LoginScreen from './screens/LoginScreen';
+import { MantineGraph } from './components/MantineGraph';
 
 
 // Sample data for the player's performance
 
-const App: React.FC = () => {
-  return (
-    <MantineProvider >
+      
 
-      <div style={{
-          display: 'flex',
-          flexDirection: 'row',
-          // justifyContent: 'center', // Centers horizontally
-          // alignItems: 'center', // Centers vertically
-          height: '100vh', 
-          width:'100%',          // Full viewport height
-          backgroundColor: '#f0f0f0', // Optional: Adds a background color
-          marginTop: '60px', // Fix margin top to 25px
-        }}>
+      const App: React.FC = () => {
+        return (
+          <MantineProvider>
+              <Router>
+                <div className="desktop-only">
+              <Header />
+                </div>
+              <div className="mobile-only">
+              <MobileHeader />
+              </div>
+              <div style={{
+              display: 'flex',
+              flexDirection: 'row',
+              height: '100vh',
+              width: '100%',
+              backgroundColor: '#f0f0f0',
+              }}>
+              {/* <div className="desktop-only">
+                <div style={{ marginLeft: '15px' }}>
+                    <LeftFooter />
+                  </div>
+                    <div style={{ position: 'absolute', right: '0', marginRight: '15px' }}>
+                    <RightFooter />
+                    </div>
+              </div> */}
+                
 
-    <div className="desktop-only" style={{ marginLeft: '15px'}}>
-          <RightFooter />
-        </div>
+              <Routes>
+                <Route
+                path="/profile"
+                element={
+                  <div className="center-container">
+                    <UserProfile />
+                    {/* Hello Worl This is Rithvik coming again... */}
+                  </div>
+                }
+                />
+                <Route
+                path="/trade"
+                element={
+                  <div className="center-container">
+                  <TradeScreen />
+                  </div>
+                }
+                />
+                <Route
+                path="/trade/graph"
+                element={
+                  <div className="center-container">
+                  <MantineGraph />
+                  </div>
+                }
+                />
+                <Route
+                path="/leagues"
+                element={
+                  <div className="center-container">
+                    <LeaguesScreen />
+                  </div>
+                }
+              />
+            <Route
+              path="/leagues/create"
+              element={
+                <div className="center-container">
+                  <CreateLeague />
+                </div>
+              }
+            />
+                <Route path="/" element={
+                <div className="center-container" >
+                  <h1 style={{ textAlign: 'center', marginBottom: '20px'}}>Welcome to the Strategic Cricket App</h1>
+                  <LoginScreen />
+                </div>
+                } />
+              </Routes>
 
-        <TradeScreen /> 
-        <div className="desktop-only" style={{ marginLeft: '15px'}}>
-          <LeftFooter />
-          </div>
-        
-
-      </div>
-       
-   
-
-     
-    </MantineProvider>
-  );
-};
+         
+              </div>
+              </Router>
+          </MantineProvider>
+        );
+      };
 
 export default App;
